522e1efc7421efa0bb8a7c491999194161322c25
PBillingsby <philbillingsby@gmail.com>
2022-10-26 13:13:17 -0400

    add config for tailwind and daisy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  reactStrictMode: false,
+  trailingSlash: true,
+  assetPrefix: './',
+  images: {
+    domains: ['arweave.net'],
+  },
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
 }
 
 module.exports = nextConfig
